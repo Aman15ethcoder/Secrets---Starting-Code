fe9299cd0ab7e4e2725c1da9ec190931fa49f393
aman15eth <amanesu15151@gmail.com>
2023-10-31 05:50:44 -0700

    app.js

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ const env= require('dotenv').config()
 const bodyParser = require('body-parser')
 const ejs = require('ejs');
 const app = express()
-const encrypt = require('mongoose-encryption')
-
+const bcrypt = require('bcrypt')
+const saltRounds=10
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-const port =7500
+const port =9000
 mongoose.connect("mongodb://127.0.0.1:27017/aut",{useNewUrlParser:true,useUnifiedTopology: true})
 const userschema = new mongoose.Schema({
     email:String,
     password:String
 })
-userschema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
 
-
+console.log(process.env.SECRET)
 const User=new mongoose.model('User',userschema)
 app.listen(port,(err)=>{
     err?console.log(err):console.log(`port listening on ${port}`)
@@ -35,17 +34,15 @@ app.get("/register",(req, res)=>{
 })
 
 app.post("/register", (req, res) => {
-    const email = req.body.username;
-    const password = req.body.password;
-    console.log(email, password);
-    
-    const newUser = new User({email: email, password: password});
-    
-    User.findOne({email: email})
+    bcrypt.hash( req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        User.findOne({email: req.body.username})
         .then(foundUser => {
             if(foundUser){
-            res.status(400).send("email already registered");
-
+                res.status(400).send("email already registered");
             } else {
                 newUser.save()
                     .then(() => res.render("secrets"))
@@ -53,6 +50,7 @@ app.post("/register", (req, res) => {
             }
         })
         .catch(err => console.log(err));
+    });
 });
 
 app.post("/login", (req, res) => {
@@ -62,11 +60,13 @@ app.post("/login", (req, res) => {
     User.findOne({email: email})
         .then(foundUser => {
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                } else {
-                    res.status(400).send("Username or password is incorrect");
-                }
+                bcrypt.compare(password, foundUser.password).then(function(result) {
+                    if(result){
+                        res.render("secrets");
+                    } else {
+                        res.status(400).send("Username or password is incorrect");
+                    }
+                });
             } else {
                 res.status(400).send("User not found");
             }
